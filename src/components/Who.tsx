e7281fd8bdcdb8ceb5af3a332ccc6a1ce7506b60
Husnain Farooq <64698888+Husnainfarooq@users.noreply.github.com>
2023-08-31 01:16:04 +0500

    Update Who.tsx

--- src/components/Who.tsx
+++ src/components/Who.tsx
@@ -14,7 +14,7 @@ const Who: FC = () => {
           data-scroll
           data-scroll-speed="2"
           className="w-[225px] h-[225px] rounded-full mr-6"
-          src="hf-portfolio/dp.png"
+          src="/hf-portfolio/dp.png"
           alt="Me"
         />
         <div>
